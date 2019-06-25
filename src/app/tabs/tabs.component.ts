44ffd8d3b88d70768ae7b292e84b651c374a97f3
idotheinternet <idotheinternet@gmail.com>
2019-06-25 15:38:04 -0600

    as;dlfkjaf

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -10,10 +10,27 @@ export class TabsComponent {
   constructor(){}
 
   @Input() level: number;
-  @Input() tabs: [];
+  @Input() tabs: object[] = [];
+
+  idx: number = 0;
 
   activateTab(idx: number) {
-    console.log(this.tabs[idx]);
+    this.idx = idx;
+  }
+
+  removeTab(idx: number) {
+    if(idx === this.idx) this.idx = idx + (idx < this.tabs.length-1 ? 0 : -1); 
+    this.tabs.splice(idx, 1);
+  }
+
+  appendTab() {
+    const entities: string[] = ['Installer ', 'Project ', 'Something '],
+    random: string = entities[Math.floor(Math.random() * entities.length)] + Math.random().toString().substring(2, 8);
+    this.tabs.push({
+      label: random,
+      isDynamic: true
+    });
+    this.idx = this.tabs.length-1;
   }
 
 }
